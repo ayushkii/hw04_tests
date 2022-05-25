8aaf44e4a300564a8b5066c4187eed29da796874
ayushkii <fac3p0k@yandex.ru>
2022-05-25 16:57:56 +0300

    a

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,11 +1,9 @@
 
 from django.contrib.auth import get_user_model
-from django.conf import settings
-from django.test import TestCase, Client, override_settings
+from django.test import TestCase, Client
 from django.urls import reverse
 from ..models import Group, Post
 from ..forms import PostForm
-from django.core.files.uploadedfile import SimpleUploadedFile
 User = get_user_model()
 
 
